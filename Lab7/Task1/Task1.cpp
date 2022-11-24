9667b5f5bbf8a2d5461ee21d4c7adc2d43ebc9a8
Ira Andieva <iraandieva7@mail.ru>
2022-11-24 17:58:12 +0300

    Task1

---
+++ Lab7/Task1/Task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void print(string s)
+{
+	cout << s << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	string m;
+	cin >> m;
+	print(m.substr(2, 3));
+
+	int index = m.find("a");
+	print(to_string(index));
+	if (index == 0) cout << "Символа а не найдено" << endl;
+}
