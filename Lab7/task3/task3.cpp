86e96092c590b3c2830494ad9436bedf84bd34eb
Ira Andieva <iraandieva7@mail.ru>
2022-12-14 18:08:08 +0300

    task 3

--- Lab7/task3/task3.cpp
+++ Lab7/task3/task3.cpp
@@ -4,6 +4,30 @@
 #include <fstream>
 using namespace std;
 
+
+void read()
+{
+	fstream fs("f.txt");
+	string line;
+	while (getline(fs, line)) cout << line << '\n';
+	fs.close();
+}
+
+void erase()
+{
+	fstream fs;
+	fs.open("f.txt", fstream::out | fstream::trunc);
+	fs.close();
+}
+
+void write(string& q)
+{
+	fstream fs;
+	fs.open("f.txt", ios::app);
+	fs << q << '\n';
+	fs.close();
+}
+
 int main()
 {
 	setlocale(LC_ALL, "");
@@ -15,16 +39,31 @@ int main()
 	float rs = trunc(s * 1000 + 0.5) / 1000;
 	cout << rs << endl;
 
-	string q;
-	cout << "Введите текст: " << endl;
-	cin >> q;
 	fstream fs;
-	fs.open("C:\\Users\\Asus\\OneDrive\\Рабочий стол\\Политех\\2 курс\\Программирование\\Labs\\Lab7\\task3\\f1.txt", std::ios::out);
+
+	fs.open("f.txt", ios::in);
 	string data;
-	string line;
-	while (getline(fs, q)) 
-	{
-		data += q + "\n";
-	}
+	fs >> data;
+	cout << data << "\n";
+	fs << "jjsbfjhbsdhv";
 	fs.close();
+
+	cout << "Введите текст: " << endl;
+	string q;
+	cin >> q;
+
+	/*fs.open("f.txt", ios::in);
+	while (getline(fs, q)) {
+		data = data +  q + "\n";
+	}
+	fs << 
+	fs.close();*/
+
+	while (true)
+	{
+		if (q == "exit") break;
+		else if (q == "read") read();
+		else if (q == "erase") erase();
+		else write(q);
+	}
 } 
